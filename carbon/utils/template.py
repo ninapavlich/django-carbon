e635cb19ac9b1248431585280480c53a274a5db4
Nina Pavlich <nina@ninalp.com>
2015-02-03 22:21:10 -0500

    tweak to template function to get templates in all sections

--- carbon/utils/template.py
+++ carbon/utils/template.py
@@ -12,25 +12,30 @@ def get_page_templates_raw(ignore_templates = None):
         ignore_templates = ()
 
     files = []
-    template_dir = settings.TEMPLATE_DIRS[0]
-    for root, dirnames, filenames in os.walk(template_dir):
-        for filename in fnmatch.filter(filenames, '*.html'):
-            raw_file_name = os.path.join(root, filename)
-            simple_path = raw_file_name.replace(template_dir+"/", "")
-            files.append(str(simple_path))
+    template_dirs = settings.TEMPLATE_DIRS
+    for template_dir in template_dirs:
+        for root, dirnames, filenames in os.walk(template_dir):
+            for filename in fnmatch.filter(filenames, '*.html'):
+                raw_file_name = os.path.join(root, filename)
+                simple_path = raw_file_name.replace(template_dir+"/", "")
+                files.append(str(simple_path))
 
     return files
 
 def get_page_templates(ignore_templates = None):
 
     raw_templates = get_page_templates_raw(ignore_templates)
-    template_dir = settings.TEMPLATE_DIRS[0]
+    output = []
 
-    def name(n):
-        return n.replace(template_dir, '')\
-            .replace('_', ' ')\
-            .replace('.html', '')\
-            .replace("-", " ")\
-            .replace("/", " - ").title()
+    template_dirs = settings.TEMPLATE_DIRS
+    for template_dir in template_dirs:
+        
+        def name(n):
+            return n.replace(template_dir, '')\
+                .replace('_', ' ')\
+                .replace('.html', '')\
+                .replace("-", " ")\
+                .replace("/", " - ").title()
+        output += [(i, name(i)) for i in raw_templates]
 
-    return [(i, name(i)) for i in raw_templates]
+    return output
