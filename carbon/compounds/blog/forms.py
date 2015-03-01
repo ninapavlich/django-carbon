b052a17603a89d69f51afd771643652693c019d5
Nina Pavlich <nina@ninalp.com>
2015-03-01 01:35:55 -0500

    update forms versions

--- carbon/compounds/blog/forms.py
+++ carbon/compounds/blog/forms.py
@@ -3,21 +3,22 @@ from ckeditor.widgets import CKEditorWidget
 from .models import *
 
 class BlogArticleAdminForm(forms.ModelForm):
-    content = forms.CharField(widget=CKEditorWidget(), required=False)
-    synopsis = forms.CharField(widget=CKEditorWidget(), required=False)
+    content = forms.CharField(widget=CKEditorWidget(config_name='blogarticle_content_ckeditor'), required=False)
+    synopsis = forms.CharField(widget=CKEditorWidget(config_name='blogarticle_synopsis_ckeditor'), required=False)
     class Meta:
         model = BlogArticle
 
 class BlogCategoryAdminForm(forms.ModelForm):
-    content = forms.CharField(widget=CKEditorWidget(), required=False)
-    synopsis = forms.CharField(widget=CKEditorWidget(), required=False)
+    content = forms.CharField(widget=CKEditorWidget(config_name='blogcategory_content_ckeditor'), required=False)
+    synopsis = forms.CharField(widget=CKEditorWidget(config_name='blogcategory_synopsis_ckeditor'), required=False)
     class Meta:
         model = BlogCategory
 
 class BlogTagAdminForm(forms.ModelForm):
-    content = forms.CharField(widget=CKEditorWidget(), required=False)
-    synopsis = forms.CharField(widget=CKEditorWidget(), required=False)
+    content = forms.CharField(widget=CKEditorWidget(config_name='blogtag_content_ckeditor'), required=False)
+    synopsis = forms.CharField(widget=CKEditorWidget(config_name='blogtag_synopsis_ckeditor'), required=False)
     class Meta:
         model = BlogTag
+
 
 
--- carbon/compounds/page/forms.py
+++ carbon/compounds/page/forms.py
@@ -3,8 +3,8 @@ from ckeditor.widgets import CKEditorWidget
 from .models import *
 
 class PageAdminForm(forms.ModelForm):
-    content = forms.CharField(widget=CKEditorWidget(), required=False)
-    synopsis = forms.CharField(widget=CKEditorWidget(), required=False)
+    content = forms.CharField(widget=CKEditorWidget(config_name='page_content_ckeditor'), required=False)
+    synopsis = forms.CharField(widget=CKEditorWidget(config_name='page_synopsis_ckeditor'), required=False)
     class Meta:
         model = Page
 
