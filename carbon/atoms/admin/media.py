e311f94a2b954ab64f1243965901ff3a1619ccd2
Nina Pavlich <nina@ninalp.com>
2015-05-12 23:04:11 -0400

    fix to bulk media upload

--- carbon/atoms/admin/media.py
+++ carbon/atoms/admin/media.py
@@ -16,29 +16,29 @@ csrf_protect_m = method_decorator(csrf_protect)
 
 
 def batch_upload_image_response(self, request):
-    # try:
-    latest_log_entry = LogEntry.objects.filter(action_flag=ADDITION).order_by('-action_time')[0]
-    ct = ContentType.objects.get_for_id(latest_log_entry.content_type_id)
-    obj = ct.get_object_for_this_type(pk=latest_log_entry.object_id)
-    if obj:
-        
-        mime = MimeTypes()
-        url = urllib.pathname2url(obj.image_url)
-        mime_type = mime.guess_type(url)
-        data = {
-            "files":[
-                {
-                    "url": obj.image_url,
-                    "thumbnailUrl": obj.thumbnail.url,
-                    "name": obj.title,
-                    "type": mime_type[0],
-                    "size": obj.image.size
-                }
-            ]
-        }
-        return HttpResponse(json.dumps(data), content_type='application/json')
-    # except:
-    #     return None
+    try:
+        latest_log_entry = LogEntry.objects.filter(action_flag=ADDITION).order_by('-action_time')[0]
+        ct = ContentType.objects.get_for_id(latest_log_entry.content_type_id)
+        obj = ct.get_object_for_this_type(pk=latest_log_entry.object_id)
+        if obj:
+            
+            mime = MimeTypes()
+            url = urllib.pathname2url(obj.image_url)
+            mime_type = mime.guess_type(url)
+            data = {
+                "files":[
+                    {
+                        "url": obj.image_url,
+                        "thumbnailUrl": obj.thumbnail.url,
+                        "name": obj.title,
+                        "type": mime_type[0],
+                        "size": obj.image.size
+                    }
+                ]
+            }
+            return HttpResponse(json.dumps(data), content_type='application/json')
+    except:
+        return None
 
 
 def batch_upload_media_response(self, request):
