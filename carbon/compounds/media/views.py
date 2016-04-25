ababe737d102b854c7dfb60ab78d97d11accdd06
Nina Pavlich <nina@ninalp.com>
2016-04-25 00:58:23 -0400

    add imagepicker that works with folders

--- carbon/compounds/media/views.py
+++ carbon/compounds/media/views.py
@@ -48,7 +48,73 @@ class ImagePickerView(BasePickerView):
     template_name = "admin/media/image_picker.html"
     model = get_model(settings.IMAGE_MODEL.split('.')[0], settings.IMAGE_MODEL.split('.')[1])
 
+class ImagePickerFolderView(BasePickerView):
+    template_name = "admin/media/image_picker_folders.html"
+    model = get_model(settings.IMAGE_MODEL.split('.')[0], settings.IMAGE_MODEL.split('.')[1])    
 
+    def get_folder_qs(self, request, folder_id=None):
+        if folder_id is None:
+          folder_id = request.POST.get('folder__id__exact', None) or request.POST.get('folder_id', None) or request.POST.get('folder', None)
+
+        if folder_id is None:
+            folder_id = request.GET.get('folder__id__exact', None) or request.GET.get('folder_id', None) or request.GET.get('folder', None)
+
+        if folder_id=="None":
+            folder_id=None
+
+        return folder_id
+
+    def get_folder(self, request, folder_id=None):
+        
+        folder_id = self.get_folder_qs(request, folder_id)
+        if folder_id and int(folder_id) >= 0:
+            return self.folder_model.objects.get(pk=folder_id)
+        return None
+
+    def ignore_folders(self, request):
+        has_query = request.GET.get('q', None) != None
+        folder_id = self.get_folder_qs(request)
+        if has_query and not folder_id:
+            return True
+
+        
+        if folder_id and int(folder_id) < 0:
+            return True
+
+        return False
+
+    def get_folders(self, current_folder=None):
+        if current_folder:
+            return self.folder_model.objects.filter(parent=current_folder)
+        else:
+            return self.folder_model.objects.filter(parent=None)
+
+    def get_all_folders(self):
+        return self.folder_model.objects.all()
+
+    def get_queryset(self):
+        qs = super(ImagePickerFolderView, self).get_queryset()
+        
+        ignore_folders = self.ignore_folders(self.request)
+        folder = self.get_folder(self.request)
+
+        if folder is None and ignore_folders is False:
+            qs = qs.filter(folder=None)
+        elif folder:
+            qs = qs.filter(folder=folder)
+
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super(ImagePickerFolderView, self).get_context_data(**kwargs)
+        request = self.request
+
+        current_folder = self.get_folder(request)
+        context['folders'] = self.get_folders(current_folder)
+        context['current_folder'] = current_folder
+        context['ignore_folders'] = self.ignore_folders(request)
+        context['top_url'] = self.request.path
+        return context
 
 class ImageBatchView(AdminBatchUploadView):      
 
